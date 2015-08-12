18b5d14d7074474aae0573378460fa2ecde86ca9
lbjworld <lbj.world@gmail.com>
2015-08-12 19:41:11 +0800

    fix textAngular css bug

--- article-manager/publish.sh
+++ article-manager/publish.sh
@@ -4,11 +4,13 @@ WORK_DIR=`pwd`
 SRC_DIR="${WORK_DIR}/ng-app/app"
 DEST_DIR="${WORK_DIR}/site/ng-dist/app"
 EXT_PATTERN=".*\(\.js\|\.css\|\.html\|\.map\)"
+INCLUDE_PATTERN=".*webfont.*"
 
 rm -rf ${DEST_DIR} && mkdir -p ${DEST_DIR} && cd ${SRC_DIR}
 grunt
 find . -type d -name '*' -exec mkdir -p ${DEST_DIR}/{} \;
 find . -type f -iregex ${EXT_PATTERN} -exec cp {} ${DEST_DIR}/{} \;
+find . -type f -iregex ${INCLUDE_PATTERN} -exec cp {} ${DEST_DIR}/{} \;
 cd -
 
 # collect all static files
